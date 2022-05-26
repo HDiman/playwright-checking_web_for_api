4aa153d29e51eea400572113506bf58f09462ff6
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-05-26 10:11:51 +0300

    Checking "body" of webpages

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 from playwright.sync_api import sync_playwright
+from datetime import datetime
 
+dt = datetime.today()
+seconds = dt.timestamp()
+print(round(seconds))
 
+url_test = "https://ria.ru/"
+
+# Taking snap shot of the web page
 with sync_playwright() as p:
     browser = p.webkit.launch()
     page = browser.new_page()
-    page.goto("https://yahoo.com")
-    page.screenshot(path="images/yahoo.jpg")
+    page.goto(url_test)
+    page.screenshot(path=f"images/example-{seconds}.png")
     browser.close()
+
+def run(playwright):
+    chromium = playwright.chromium
+    browser = chromium.launch()
+    page = browser.new_page()
+
+    # Subscribe to "request" and "response" events
+    page.on("request", lambda request: print(">>", request.method, request.url))
+    page.on("response", lambda response: print("<<", response.url, response.body))
+    page.goto(url_test)
+    browser.close()
+
+with sync_playwright() as playwright:
+    run(playwright)
--- playground.py
+++ playground.py
@@ -1,15 +1,24 @@
 from playwright.sync_api import sync_playwright
+from datetime import datetime
 
+# Time in seconds
+dt = datetime.today()
+seconds = dt.timestamp()
+print(round(seconds))
+
+
+# Searching title info on webpage
 with sync_playwright() as p:
     browser = p.chromium.launch()
     page = browser.new_page()
-    page.goto("http://playwright.dev")
+    page.goto("http://yahoo.com")
     print(page.title())
     browser.close()
 
+# Taking snap shot of the webpage
 with sync_playwright() as p:
     browser = p.webkit.launch()
     page = browser.new_page()
-    page.goto("http://whatsmyuseragent.org/")
-    page.screenshot(path="images/example_my_agent.png")
+    page.goto("http://jhnwr.com")
+    page.screenshot(path="images/example.png")
     browser.close()
